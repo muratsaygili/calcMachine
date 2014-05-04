a5406901298b43d7d228cb8f7519e5066cc61dcc
muratsaygili <muratsaygili1@gmail.com>
2014-05-04 18:07:01 +0300

    Create calc.java

---
+++ calc.java
@@ -0,0 +1,31 @@
+package calcMachine;
+
+public class calc {
+	
+	
+	
+	public static int toplama(double a, double b)
+	{
+		return (int) (a+b);
+	}
+	public static int cikarma(double a, double b)
+	{
+		
+		return (int) (a-b);
+	}
+	public static int carpma(double a, double b)
+	{
+		
+		return (int) (a*b);
+	}
+	public static float bolme(double a, double b)
+	{
+		
+		return (float) (a/b);
+	}
+	public static void goster(float i)
+	{
+		System.out.println(i);		
+	}
+
+}
